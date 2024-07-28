a828d2cb1fb09f12e199e673955700b33714e665
smiiith <smiiith@gmail.com>
2024-07-28 12:50:21 -0700

    fix: build issues.

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,5 +3,6 @@
 import { ThemeProvider } from 'next-themes'
 
 export function Providers({ children }: { children: React.ReactNode }) {
+    //@ts-ignore
     return <ThemeProvider attribute="class" defaultTheme='system' enableSystem>{children}</ThemeProvider>
 }
