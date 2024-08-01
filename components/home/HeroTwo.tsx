d7f1277a9388a2c761c8b1fe8c1296c763f0383b
smiiith <smiiith@gmail.com>
2024-08-01 08:20:24 -0700

    fix: remove troublesome chars

--- components/home/HeroTwo.tsx
+++ components/home/HeroTwo.tsx
@@ -5,7 +5,7 @@ const HeroTwo = () => {
     return (
         <div className="">
             <h1 className="text-5xl mb-6">Sub Header</h1>
-            <p className="my-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p className="my-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 
             <Button className="my-6">Sign Me Up</Button>
         </div>
