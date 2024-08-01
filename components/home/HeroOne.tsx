079848b69491ab86c3f9230d902e9f5c291df0c4
smiiith <smiiith@gmail.com>
2024-08-01 08:14:07 -0700

    feature: Sample parralax home page. nan-24.

---
+++ components/home/HeroOne.tsx
@@ -0,0 +1,14 @@
+import { Button } from "../ui/button";
+
+const HeroOne = () => {
+
+    return (
+        <div className="">
+            <h1 className="text-7xl mb-6">SyncNanny</h1>
+            <p className="my-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+            <Button className="my-6">Learn More</Button>
+        </div>
+    )
+}
+export default HeroOne;
