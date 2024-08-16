72f30fc16521e5fd749f320637dc7cc4c8f6342a
smiiith <smiiith@gmail.com>
2024-08-16 07:55:40 -0700

    fix: apostrophe.

--- components/home/HeroTwo.tsx
+++ components/home/HeroTwo.tsx
@@ -9,7 +9,7 @@ const HeroTwo = () => {
                 SyncNanny is here, 24/7, to Watch Over Your Calendars
             </h2>
             <p className="my-6">
-                Despite software to link them, it's a fact that calendars across the major booking platforms lose their sync. This results in costly double bookings, poor customer experiences, lower ratings, lost bookings, and host status reduction. Sync Nanny is here, working 24/7 to watch over your calendars and help you successfully manage your listings. Start your FREE TRIAL today.
+                Despite software to link them, it&apos;s a fact that calendars across the major booking platforms lose their sync. This results in costly double bookings, poor customer experiences, lower ratings, lost bookings, and host status reduction. Sync Nanny is here, working 24/7 to watch over your calendars and help you successfully manage your listings. Start your FREE TRIAL today.
             </p>
 
             <Button className="my-6 bg-secondary">Pricing Info</Button>
