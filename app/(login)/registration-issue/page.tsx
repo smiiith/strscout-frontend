3d08abb7b916d2c28b2ac2a1d856aa40a58817f8
smiiith <smiiith@gmail.com>
2024-09-22 23:51:42 -0700

    fix: encoding issues

--- app/(login)/registration-issue/page.tsx
+++ app/(login)/registration-issue/page.tsx
@@ -12,7 +12,7 @@ const RegistrationIssuePage = () => {
 
             <p className="w-[400px] py-4">Please make sure you have entered the correct email address and try again.</p>
 
-            <p className="w-[400px] pb-6">If you think you have already created an account, click on "Forgot Password" on the registration page (after clicking the button below.).</p>
+            <p className="w-[400px] pb-6">If you think you have already created an account, click on Forgot Password on the registration page (after clicking the button below.).</p>
 
             <Button onClick={() => router.push('/register')}>Try Again</Button>
 
