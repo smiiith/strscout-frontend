c67cc1993cd3ceafe479c44a372492557fa88799
smiiith <smiiith@gmail.com>
2025-02-14 09:25:39 -0800

    chore: New UI on login and register pages. Resizeable title on home page. (#17)

--- app/(login)/login/page.tsx
+++ app/(login)/login/page.tsx
@@ -4,45 +4,72 @@ import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
+import Image from 'next/image'
+import HeaderNav from '@/components/header'
 
 export default function LoginPage() {
 
   return (
-    <Card className="mx-auto max-w-sm">
-      <form>
-        <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>Enter your email below to login to your account</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="grid gap-8">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              {/* <input id="email" name="email" type="email" required /> */}
-              <Input id="email" name="email" type="email" placeholder="m@example.com" required />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <Link href="/password-request" className="ml-auto inline-block text-sm underline" prefetch={false}>
-                  Forgot your password?
-                </Link>
+    // <div className="flex flex-col">
+    //   <div className="flex-grow">
+    //     <div className="container mx-auto p-0 max-w-7xl bg-background">
+
+    //       <HeaderNav />
+
+    //       <div className="px-6">
+    <div className="">
+      <Image
+        src="/STR-Feedback-Genius-Logo-single-line.png"
+        alt="STR Feedback Genius"
+        width="754"
+        height="72"
+        className="w-[754] h-auto my-6"
+      />
+      <Card className="max-w-sm bg-background border-none">
+        <form>
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>Enter your email below to login to your account</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid gap-8">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                {/* <input id="email" name="email" type="email" required /> */}
+                <Input id="email" name="email" type="email" placeholder="m@example.com" required />
               </div>
-              {/* <input id="password" name="password" type="password" required /> */}
-              <Input id="password" name="password" type="password" required />
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <Link href="/password-request" className="ml-auto inline-block text-sm underline" prefetch={false}>
+                    Forgot your password?
+                  </Link>
+                </div>
+                {/* <input id="password" name="password" type="password" required /> */}
+                <Input id="password" name="password" type="password" required />
+              </div>
+              <Button
+                type="submit"
+                className="w-fit px-8"
+                formAction={login}
+              >
+                Log in
+              </Button>
             </div>
-            <Button type="submit" className="w-full" formAction={login} variant={"outline"}>
-              Login
-            </Button>
-          </div>
-          <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link href="/register" className="underline" prefetch={false}>
-              Create one
-            </Link>
-          </div>
-        </CardContent>
-      </form>
-    </Card >
+            <div className="mt-4 text-sm">
+              Don&apos;t have an account?{" "}
+              <Link href="/register" className="underline" prefetch={false}>
+                Create one
+              </Link>
+            </div>
+          </CardContent>
+        </form>
+      </Card >
+    </div>
+
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
