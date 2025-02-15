5c7d2ca7c54b3b725a0b12d97eca449a9600c51e
smiiith <smiiith@gmail.com>
2025-02-15 11:31:58 -0800

    chore: str-33. New design on password reset page. Add text to register page.

--- app/(login)/password-request/page.tsx
+++ app/(login)/password-request/page.tsx
@@ -4,36 +4,49 @@ import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
+import Image from 'next/image'
+
 
 export default function PasswordReset() {
 
   return (
-    <Card className="mx-auto max-w-sm">
-      <form id="rester_pwd_form">
-        <CardHeader>
-          <CardTitle className="text-2xl">Password Reset Request</CardTitle>
-          <CardDescription>Enter your email below to request a password reset. You will receive an email with a link to reset your password.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="grid gap-8">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" name="email" type="email" placeholder="m@example.com" required />
-            </div>
-            <Button type="submit" id="rester_pwd_btn" className="w-full" formAction={resetPassword} variant={"outline"}>
-              Send
-            </Button>
-          </div>
-          <div className="mt-4 text-center text-sm">
-            <Link href="/login" className="underline" prefetch={false}>
-              Log In
-            </Link>
-          </div>
-        </CardContent>
-      </form>
+    <>
+      <Image
+        src="/STR-Feedback-Genius-Logo-single-line.png"
+        alt="STR Feedback Genius"
+        width="754"
+        height="72"
+        className="w-[754] h-auto my-6"
+      />
 
-      <script dangerouslySetInnerHTML={{
-        __html: `
+      <h1 className="text-2xl mb-6">Password Reset Request</h1>
+
+      <div className="text-xl mb-6">
+        Enter your email below to request a password reset. You will receive an email with a link to reset your password.
+      </div>
+
+      <div className="">
+        <form id="rester_pwd_form" className="pb-6">
+          <div>
+            <div className="grid gap-8">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" name="email" type="email" placeholder="m@example.com" required className="w-1/4" />
+              </div>
+              <Button type="submit" id="rester_pwd_btn" className="w-fit" formAction={resetPassword}>
+                Send
+              </Button>
+            </div>
+            <div className="mt-4 text-sm">
+              <Link href="/login" className="underline" prefetch={false}>
+                Log In
+              </Link>
+            </div>
+          </div>
+        </form>
+
+        <script dangerouslySetInnerHTML={{
+          __html: `
           document.getElementById('rester_pwd_form').addEventListener('submit', function(event) {
             const button = document.getElementById('rester_pwd_btn');
             button.disabled = true;
@@ -47,8 +60,10 @@ export default function PasswordReset() {
             }
           });
         `
-      }} />
+        }} />
 
-    </Card>
+      </div>
+    </>
+
   )
 }
