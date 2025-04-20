606ad6eadc8748711115c4ba6d4a8133d8e84a1f
smiiith <smiiith@gmail.com>
2025-04-20 16:34:36 -0700

    chore: add whitelist back with an on/off env var.

--- app/(no-auth)/no-registration/page.tsx
+++ app/(no-auth)/no-registration/page.tsx
@@ -1,7 +1,7 @@
 
 const NoRegistrationPage = () => {
     return (
-        <div>
+        <div className="pt-8 pb-[300px]">
             <h1 className="text-3xl mb-6">Registration is currently closed</h1>
 
             <p>Thanks for your interest in STR Genius. Check back soon when we open for new users!</p>
