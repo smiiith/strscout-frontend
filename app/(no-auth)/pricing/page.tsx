a25a887c0e89824c05763040f7c13cbc9ea81f1a
smiiith <smiiith@gmail.com>
2025-06-28 14:21:06 -0700

    feat: str-121. Set up stripe webhook and checkout form integration. (#67)

--- app/(no-auth)/pricing/page.tsx
+++ app/(no-auth)/pricing/page.tsx
@@ -1,17 +1,23 @@
 "use client";
 
 import ServerBusyDialog from "@/components/server-busy";
-import { Button } from "@/components/ui/button"
-import Image from "next/image"
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
+import StripeCheckoutButton from "@/components/stripe-checkout-button";
 
 export default function FeatureSections() {
   const router = useRouter();
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="flex flex-col md:flex-row md:divide-x md:divide-gray-300">
+      <StripeCheckoutButton
+        priceId="price_1Rf3qeRQojxLKgwUSlmUkEbH"
+        buttonText="Subscribe Now"
+        className="bg-primary text-lg absolute bottom-0"
+      />
 
+      <div className="flex flex-col md:flex-row md:divide-x md:divide-gray-300">
         {/* STR Feedback Genius Section */}
         <FeatureSection
           imageSrc="/images/str-feedback-genius-logo-stacked.png"
@@ -33,7 +39,7 @@ export default function FeatureSections() {
           price="Free"
           buttonText="Sign Up Free Now"
           buttonAction={() => {
-            router.push("/register")
+            router.push("/register");
           }}
         />
 
@@ -45,18 +51,19 @@ export default function FeatureSections() {
           description={
             <div className="space-y-4 my-4">
               <p>
-                Success in today's STR market depends on knowing what works.
-                Are your amenities competitive? Are your photos compelling? What about your policies?
+                Success in today's STR market depends on knowing what works. Are
+                your amenities competitive? Are your photos compelling? What
+                about your policies?
               </p>
+              <p>That's where STR Market Spy comes in.</p>
               <p>
-                That's where STR Market Spy comes in.
+                Just like STR Feedback Genius, STR Market Spy digs deep — but
+                this time, into the listings around you. It analyzes nearby
+                competitors across occupancy, amenities, photos, descriptions,
+                policies & more — uncovering the secret sauce behind their
+                success.
               </p>
-              <p>
-                Just like STR Feedback Genius, STR Market Spy digs deep — but this time, into the listings around you. It analyzes nearby competitors across occupancy, amenities, photos, descriptions, policies & more — uncovering the secret sauce behind their success.
-              </p>
-              <p>
-                See the market clearly. Act with confidence.
-              </p>
+              <p>See the market clearly. Act with confidence.</p>
             </div>
           }
           price=""
@@ -72,7 +79,10 @@ export default function FeatureSections() {
           description={
             <ul className="mb-6 space-y-2">
               <li>• We find nearby similar properties</li>
-              <li>• How are they performing today, this month, and the upcoming months</li>
+              <li>
+                • How are they performing today, this month, and the upcoming
+                months
+              </li>
               <li>• What features do they have?</li>
               <li>• What amenities do they have?</li>
               <li>• What is the quality of their listing?</li>
@@ -81,20 +91,26 @@ export default function FeatureSections() {
           price=""
           buttonText="Apply for Beta Test"
           buttonAction={() => {
-            router.push("/contact-us")
+            router.push("/contact-us");
           }}
         />
-
       </div>
     </div>
-  )
+  );
 }
 
-const FeatureSection = ({ imageSrc, alt, title, description, price, buttonText, buttonAction }) => {
+const FeatureSection = ({
+  imageSrc,
+  alt,
+  title,
+  description,
+  price,
+  buttonText,
+  buttonAction,
+}) => {
   return (
     <section className="flex-1 py-6 px-0 md:p-6 relative mb-8 md:mb-0">
       <div className="mb-4 flex items-center gap-4">
-
         <Image
           src={imageSrc}
           alt={alt}
@@ -103,11 +119,8 @@ const FeatureSection = ({ imageSrc, alt, title, description, price, buttonText, 
           quality={100}
           className="w-auto h-[80px] mb-4"
         />
-
       </div>
-      <h3 className="mb-4 text-xl font-bold">
-        {title}
-      </h3>
+      <h3 className="mb-4 text-xl font-bold">{title}</h3>
       <div className="mb-2 text-lg">{description}</div>
 
       <p className="mb-8 text-xl font-bold">{price}</p>
@@ -118,16 +131,19 @@ const FeatureSection = ({ imageSrc, alt, title, description, price, buttonText, 
             className="bg-primary text-lg absolute bottom-0"
             onClick={() => {
               if (buttonAction) {
-                buttonAction()
+                buttonAction();
               }
             }}
           >
             {buttonText}
           </Button>
         ) : (
-          <ServerBusyDialog buttonText={buttonText} triggerStyles="bg-primary text-lg absolute bottom-0" />
+          <ServerBusyDialog
+            buttonText={buttonText}
+            triggerStyles="bg-primary text-lg absolute bottom-0"
+          />
         )}
       </>
     </section>
-  )
-}
+  );
+};
