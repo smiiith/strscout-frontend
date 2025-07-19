017268428416c9dcb1003dbe2e5477901cc8ca51
smiiith <smiiith@gmail.com>
2025-07-19 12:12:51 -0700

    chore: str-172. Add date to market spy report. Clean up some layout and UI. (#77)

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,21 +1,25 @@
-import { type ClassValue, clsx } from "clsx"
+import { type ClassValue, clsx } from "clsx";
 import { formatInTimeZone } from "date-fns-tz";
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export const formatDate = (date: Date) => {
   const dateObj = new Date(date);
   const timeZone = "America/Los_Angeles";
-  const zonedDate = formatInTimeZone(dateObj, timeZone, "MM/dd/yyyy hh:mm aaaa zzz");
+  const zonedDate = formatInTimeZone(
+    dateObj,
+    timeZone,
+    "MM/dd/yyyy hh:mm aaaa zzz"
+  );
   return zonedDate;
-}
+};
 
 export const formatDateNoTime = (date: Date) => {
   const dateObj = new Date(date);
   const timeZone = "America/Los_Angeles";
   const zonedDate = formatInTimeZone(dateObj, timeZone, "MM/dd/yyyy");
   return zonedDate;
-}
+};
