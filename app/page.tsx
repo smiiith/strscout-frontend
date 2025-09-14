9621a5b0fcf12b26e53ac4746524727cc3f30be1
smiiith <smiiith@gmail.com>
2025-09-13 20:27:58 -0700

    chore: str-218. home page updates. (#102)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import "./home.css";
-import HeaderNav from "@/components/header";
-import ParallaxContent from "./parallax-content";
+import HomePage from "@/components/home-page";
 import { createClient } from "@/utils/supabase/server";
 import { getUserWithPlan } from "./(authenticated)/utils";
 
@@ -16,16 +14,5 @@ export default async function Home() {
     userProfile = await getUserWithPlan(user.id);
   }
 
-  return (
-    <div className="flex flex-col">
-      <div className="flex-grow">
-        <div className="container mx-auto p-0 max-w-7xl bg-background">
-          <>
-            <HeaderNav user={userProfile} />
-            <ParallaxContent />
-          </>
-        </div>
-      </div>
-    </div>
-  );
+  return <HomePage user={userProfile} />;
 }
